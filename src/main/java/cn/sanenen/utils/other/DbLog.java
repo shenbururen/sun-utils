895de904fb2f8c6f5eea9d2540ebab1e43ebe94d
sun <sun@sanenen.cn>
2021-09-02 15:37:03 +0800

    日志数据备份工具类

---
+++ src/main/java/cn/sanenen/utils/other/DbLog.java
@@ -0,0 +1,14 @@
+package cn.sanenen.utils.other;
+
+import cn.hutool.log.Log;
+
+/**
+ * 有错误时，使用此类将数据持久化到本地
+ */
+public class DbLog {
+	private static final Log log = Log.get();
+
+	public static void log(String msg, Object... objects) {
+		log.info(msg, objects);
+	}
+}
