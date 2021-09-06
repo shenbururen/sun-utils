42dd458e393f2cf0cd368447fd194bb277707ed8
sun <sun@sanenen.cn>
2021-09-06 18:05:11 +0800

    增加JacksonUtil 测试用例

---
+++ src/test/java/cn/sanenen/utils/json/JacksonUtilTest.java
@@ -0,0 +1,18 @@
+package cn.sanenen.utils.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author sun
+ * @date 2021-09-06
+ **/
+public class JacksonUtilTest {
+
+	@Test
+	public void jacksonUtilTest() {
+		JsonNode jsonNode = JacksonUtil.toObj("{\"key1\":\"123\"}");
+		Assert.assertEquals(jsonNode.get("key1").asInt(), 123);
+	}
+}
