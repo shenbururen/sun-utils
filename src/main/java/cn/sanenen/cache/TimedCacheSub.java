05270d3a85a13d02bb1fc54fcebaf1b47844d103
sun <sun@sanenen.cn>
2021-09-08 16:21:14 +0800

    扩展hutool的过期缓存。

---
+++ src/main/java/cn/sanenen/cache/TimedCacheSub.java
@@ -0,0 +1,28 @@
+package cn.sanenen.cache;
+
+import cn.hutool.cache.impl.CacheObj;
+import cn.hutool.cache.impl.TimedCache;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 扩展hutool包的过期缓存，增加getAndRemove方法。固定使用ConcurrentHashMap。
+ * @author sun
+ * @date 2021-09-08
+ **/
+public class TimedCacheSub<K, V> extends TimedCache<K, V> {
+	public TimedCacheSub(long timeout) {
+		super(timeout, new ConcurrentHashMap<>());
+	}
+
+	/**
+	 * 获取并移除缓存。
+	 */
+	public V getAndRemove(K key) {
+		CacheObj<K, V> co = super.cacheMap.remove(key);
+		if (co != null) {
+			return co.getValue();
+		}
+		return null;
+	}
+}
