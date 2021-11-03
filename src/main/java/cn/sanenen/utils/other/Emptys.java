dd516064ee5f25ce11d12767f7a99548df541eb5
sun <sun@sanenen.cn>
2021-11-03 14:19:31 +0800

    定义一些空常量。

---
+++ src/main/java/cn/sanenen/utils/other/Emptys.java
@@ -0,0 +1,14 @@
+package cn.sanenen.utils.other;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 定义一些空常量
+ * @author sun
+ * @date 2021-10-29
+ **/
+public interface Emptys {
+	String STR = StrUtil.EMPTY;
+	byte[] BYTES = new byte[0];
+	String[] STR_ARRAY = new String[0];
+}
