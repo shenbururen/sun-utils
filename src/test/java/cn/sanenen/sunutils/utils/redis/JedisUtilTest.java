f372f0a88063b0475edfcf735dbd65cebf35bc61
sun <sun@sanenen.cn>
2023-12-07 16:23:30 +0800

    添加setnxex测试方法。

--- src/test/java/cn/sanenen/sunutils/utils/redis/JedisUtilTest.java
+++ src/test/java/cn/sanenen/sunutils/utils/redis/JedisUtilTest.java
@@ -1,11 +1,13 @@
 package cn.sanenen.sunutils.utils.redis;
 
 import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.db.nosql.redis.RedisDS;
-import cn.sanenen.sunutils.utils.redis.JedisUtil;
 import com.alibaba.fastjson.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +18,14 @@ import java.util.List;
 public class JedisUtilTest {
 	private final JedisUtil test = new JedisUtil(RedisDS.create("test"));
 
+	@Test
+	public void testSetnxex() {
+		String key = "test:setnxex";
+		Assert.assertTrue(test.setnxex(key, "1", Duration.ofSeconds(10).getSeconds()));
+		Assert.assertFalse(test.setnxex(key, "1", Duration.ofSeconds(10).getSeconds()));
+		ThreadUtil.sleep(Duration.ofSeconds(10).toMillis());
+		Assert.assertTrue(test.setnxex(key, "1", Duration.ofSeconds(10).getSeconds()));
+	}
 	@Test
 	public void test0() {
 		test.hincrByFloat("aafd", "der",2.85);
